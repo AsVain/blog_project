40a5f36cb10e921b0a6eb04d2d44b7a2ac900441
AsVain <przemorek11@gmail.com>
2024-05-23 07:49:57 +0200

    oh god it works - all posts page and links

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path\
+from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page")    #slug concept - slug to koncowka adresu, sluyg transformer (slug: sprawdza, czy ta część, ostatnia część ścieżki ma taki format: xxx-xxx-xxxxxxx-xxxxxxxxxx-xxxx-xxxxxxx)
-}
+]
