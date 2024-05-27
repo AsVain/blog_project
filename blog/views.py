0626af2d21bfd2318a292d9ab9eed0cf1814c782
AsVain <przemorek11@gmail.com>
2024-05-27 08:13:11 +0200

    dynamic variables part 2

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ all_posts = [       #lista postów, każdy post będzie słownikiem
         "date": date(2024, 5, 23),
         "title": "Mountain Hiking",
         "exerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "contnet": """
+        "content": """
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus erat vel justo vulputate laoreet. Sed sagittis justo sodales pulvinar blandit. Donec sit amet sagittis augue. Aenean euismod faucibus arcu, at porta justo hendrerit nec. Fusce commodo lorem nec leo pulvinar, et semper tortor pretium. Nunc viverra nisi eu justo facilisis, id viverra sapien commodo. Cras vulputate, urna sed condimentum finibus, nisl nunc imperdiet lorem, interdum placerat dolor leo ut justo. Aenean non nunc nulla. Integer convallis fermentum aliquet. Quisque laoreet libero a nulla condimentum lobortis. Proin rutrum libero tortor, a vehicula elit feugiat et. Nam eu magna ac dui vulputate molestie ac vel lorem. Aliquam ac sagittis risus, et placerat magna.
             
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus erat vel justo vulputate laoreet. Sed sagittis justo sodales pulvinar blandit. Donec sit amet sagittis augue. Aenean euismod faucibus arcu, at porta justo hendrerit nec. Fusce commodo lorem nec leo pulvinar, et semper tortor pretium. Nunc viverra nisi eu justo facilisis, id viverra sapien commodo. Cras vulputate, urna sed condimentum finibus, nisl nunc imperdiet lorem, interdum placerat dolor leo ut justo. Aenean non nunc nulla. Integer convallis fermentum aliquet. Quisque laoreet libero a nulla condimentum lobortis. Proin rutrum libero tortor, a vehicula elit feugiat et. Nam eu magna ac dui vulputate molestie ac vel lorem. Aliquam ac sagittis risus, et placerat magna.
@@ -25,7 +25,7 @@ all_posts = [       #lista postów, każdy post będzie słownikiem
         "date": date(2024, 5, 21),
         "title": "Mountain Hiking",
         "exerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "contnet": """
+        "content": """
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus erat vel justo vulputate laoreet. Sed sagittis justo sodales pulvinar blandit. Donec sit amet sagittis augue. Aenean euismod faucibus arcu, at porta justo hendrerit nec. Fusce commodo lorem nec leo pulvinar, et semper tortor pretium. Nunc viverra nisi eu justo facilisis, id viverra sapien commodo. Cras vulputate, urna sed condimentum finibus, nisl nunc imperdiet lorem, interdum placerat dolor leo ut justo. Aenean non nunc nulla. Integer convallis fermentum aliquet. Quisque laoreet libero a nulla condimentum lobortis. Proin rutrum libero tortor, a vehicula elit feugiat et. Nam eu magna ac dui vulputate molestie ac vel lorem. Aliquam ac sagittis risus, et placerat magna.
             
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus erat vel justo vulputate laoreet. Sed sagittis justo sodales pulvinar blandit. Donec sit amet sagittis augue. Aenean euismod faucibus arcu, at porta justo hendrerit nec. Fusce commodo lorem nec leo pulvinar, et semper tortor pretium. Nunc viverra nisi eu justo facilisis, id viverra sapien commodo. Cras vulputate, urna sed condimentum finibus, nisl nunc imperdiet lorem, interdum placerat dolor leo ut justo. Aenean non nunc nulla. Integer convallis fermentum aliquet. Quisque laoreet libero a nulla condimentum lobortis. Proin rutrum libero tortor, a vehicula elit feugiat et. Nam eu magna ac dui vulputate molestie ac vel lorem. Aliquam ac sagittis risus, et placerat magna.
@@ -40,7 +40,7 @@ all_posts = [       #lista postów, każdy post będzie słownikiem
         "date": date(2024, 5, 26),
         "title": "Mountain Hiking",
         "exerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "contnet": """
+        "content": """
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus erat vel justo vulputate laoreet. Sed sagittis justo sodales pulvinar blandit. Donec sit amet sagittis augue. Aenean euismod faucibus arcu, at porta justo hendrerit nec. Fusce commodo lorem nec leo pulvinar, et semper tortor pretium. Nunc viverra nisi eu justo facilisis, id viverra sapien commodo. Cras vulputate, urna sed condimentum finibus, nisl nunc imperdiet lorem, interdum placerat dolor leo ut justo. Aenean non nunc nulla. Integer convallis fermentum aliquet. Quisque laoreet libero a nulla condimentum lobortis. Proin rutrum libero tortor, a vehicula elit feugiat et. Nam eu magna ac dui vulputate molestie ac vel lorem. Aliquam ac sagittis risus, et placerat magna.
             
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus erat vel justo vulputate laoreet. Sed sagittis justo sodales pulvinar blandit. Donec sit amet sagittis augue. Aenean euismod faucibus arcu, at porta justo hendrerit nec. Fusce commodo lorem nec leo pulvinar, et semper tortor pretium. Nunc viverra nisi eu justo facilisis, id viverra sapien commodo. Cras vulputate, urna sed condimentum finibus, nisl nunc imperdiet lorem, interdum placerat dolor leo ut justo. Aenean non nunc nulla. Integer convallis fermentum aliquet. Quisque laoreet libero a nulla condimentum lobortis. Proin rutrum libero tortor, a vehicula elit feugiat et. Nam eu magna ac dui vulputate molestie ac vel lorem. Aliquam ac sagittis risus, et placerat magna.
@@ -63,7 +63,12 @@ def starting_page(request):
     })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html",{
+        "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    indentified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": indentified_post
+    })
